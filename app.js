f153da70f23ece0ca6703578036e3ad9b36dd72b
AlvaroLou <alvaro.lourenco46@gmail.com>
2021-09-08 19:58:47 -0300

    Aula 2

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-var mostrarSite = true;
+var calculadora = require('./calculadora');
+
+console.log(calculadora.name);
+console.log(calculadora.soma(10, 20));
+console.log(calculadora.sub(10, 20));
+console.log(calculadora.multi(10, 20));
+console.log(calculadora.div(10, 20));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var mostrarSite = true;
 const site = "https://www.twitch.tv/joy__boy__";
 
 console.log("Hello World!!");
@@ -7,4 +28,4 @@ console.log("E eu estou aprendendo Node.js com o Guia do Programador")
 
 if (mostrarSite) {//MOSTRA SITE
   console.log(site);//PRINTA SITE
-}
+}*/
---
+++ calculadora.js
@@ -0,0 +1,25 @@
+var name = "Calculadora 1k";
+
+module.exports = {
+  soma,
+  sub,
+  multi,
+  div,
+  name
+}
+
+function soma(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function multi(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
