ec3edf7f90ca69d9b561301068eacb713192e099
AlvaroLou <alvaro.lourenco46@gmail.com>
2021-09-15 19:44:42 -0300

    Rotas

--- Express/index.js
+++ Express/index.js
@@ -1,6 +1,18 @@
 const express = require('express'); //Importando o express
 const app = express(); //Iniciando o express
 
+app.get("/", function (req, res) {//Faz uma requisição e manda uma resposta
+  res.send("Bem vindo a minha live!");
+});
+
+app.get("/blog", function (req, res) {
+  res.send("Bom vindo ao meu blog!");
+});
+
+app.get("/canal/youtube", function (req, res) {
+  res.send("<h1>Bem vindo ao meu canal!</h1>");
+});
+
 app.listen(4000, function (erro) {
   if (erro) {
     console.log("Ocorreu um erro");
