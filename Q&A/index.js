94348df08c5a0eff17c69cd0c2d5dad2c62c7b29
AlvaroLou <alvaro.lourenco46@gmail.com>
2023-03-29 14:32:11 -0300

    Continue project

--- Q&A/index.js
+++ Q&A/index.js
@@ -5,29 +5,12 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get("/:nome/:lang",(req,res)=>{
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
+app.get("/",(req,res)=>{
+    res.render("index");
+});
 
-    var produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Coca-cola", preco: 5},
-        {nome: "Leite", preco: 1.45},
-        {nome: "Carne", preco: 15},
-        {nome: "Redbull", preco: 6},
-        {nome: "Nescau", preco: 4},
-    ]
-
-    res.render("index",{
-        nome: nome,
-        lang: lang,
-        nick: "Joy Boy",
-        canal: "https://www.twitch.tv/joy__boy__",
-        seguidores: 61,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+app.get("/perguntar",(req,res)=>{
+    res.render("perguntar");
 });
 
 app.listen(8080,()=>{console.log("App rodando!");});
